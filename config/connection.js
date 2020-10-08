8f464fed78db906a6a67adf1a753734856ae71ae
Jordan Muncrief <jordean89@gmail.com>
2020-10-08 11:03:45 -0600

    Fixed typos in config

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSB_URL) {
-  connection = mysql.createConnection(process.envJAWSDB_URL)
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   connection = mysql.createConnection({
     host: "localhost",
