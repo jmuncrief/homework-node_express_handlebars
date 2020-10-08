226bcb88fc999f61031f5433d7a09793a3435c87
Jordan Muncrief <jordean89@gmail.com>
2020-10-07 23:25:58 -0600

    Complete. Burgers now devour

---
+++ controllers/burgerController.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var burger = require('../models/burger.js'); 
+router.get('/', function(req, res) {
+	res.redirect('/index');
+});
+
+router.get('/index', function(req, res) {
+	burger.selectAll(function(data) {
+		var hbsObject = {burgers: data};
+		console.log(hbsObject);
+		res.render('index', hbsObject);
+	});
+});
+
+router.post('/burgers/insertOne', function(req, res) {
+	burger.insertOne(['name', 'devoured'], [req.body.name, false], function() {
+		res.redirect('/index');
+	});
+});
+
+router.post('/burgers/updateOne/:id', function(req, res) {
+	var condition = 'id = ' + req.params.id;
+	console.log('condition', condition);
+
+	burger.updateOne({devoured: req.body.devour}, condition, function() {
+		res.redirect('/index');
+	});
+});
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Green Chile Cheeseburger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Impossible Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Bison Burger', false);
